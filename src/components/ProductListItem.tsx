71e1493f6d3199ffd8ec2aaeae6d0208bb7a4d1f
Orjihenry <orjihen@yahoo.com>
2024-04-20 22:32:52 -0230

    Refactor menu screen to use FlatList for rendering products

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,11 +1,19 @@
-import Colors from '@/src/constants/Colors';
+import Colors from '@constants/Colors';
+import { Product } from '@src/types';
 import { StyleSheet, Text, View, Image} from 'react-native';
 
-const ProductListItem = ({ product }) => {
+export const defaultImage = 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png';
+
+type ProductListItemProps = {
+    product: Product;
+  };
+  
+const ProductListItem = ({ product }: ProductListItemProps) => {
   console.log(product)
   return (
     <View style={styles.container}>
-      <Image source={{ uri: product.image }} style={styles.image} />
+      <Image source={{ uri: product.image || defaultImage }} style={styles.image}
+        resizeMode="contain" />
 
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}> Price: ${product.price}</Text>
@@ -20,6 +28,8 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     padding: 10,
     borderRadius: 20,
+    flex: 1,
+    maxWidth: '50%'
   },
   image: {
     width: '100%',
